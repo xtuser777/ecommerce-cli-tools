69a40e076b303c42ff9d09799f5fada87c10ae80
Lucas <lucaoxt@gmail.com>
2024-11-18 10:41:52 -0300

    fix architecture layers

--- src/index.js
+++ src/index.js
@@ -1,19 +1,3 @@
-import prompt from "prompt";
-
-import promptSchemaMain from "./schema/main.js";
-
-import createQRCode from "./services/qr-code/create.js";
-import createPassword from "./services/password/create.js";
-
-async function main() {
-  prompt.get(promptSchemaMain, async (err, choose) => {
-    if (err) console.log(err);
-
-    if (choose.select === '1') await createQRCode();
-    if (choose.select === '2') await createPassword();
-  });
-
-  prompt.start();
-}
+import main from "./services/prompt/main.js";
 
 main();
---
+++ src/services/prompt/main.js
@@ -0,0 +1,18 @@
+import prompt from "prompt";
+
+import promptSchemaMain from "../../schema/main.js";
+
+import createQRCode from "../qr-code/create.js";
+
+async function main() {
+  prompt.get(promptSchemaMain, async (err, choose) => {
+    if (err) console.log(err);
+
+    if (choose.select === "1") await createQRCode();
+    if (choose.select === "2") await createPassword();
+  });
+
+  prompt.start();
+}
+
+export default main;
